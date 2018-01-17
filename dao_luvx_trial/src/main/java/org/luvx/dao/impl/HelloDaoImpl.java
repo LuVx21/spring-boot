13b096dd84fa3a2525e8671ca8a22c8e1f832c99
RenXie <yeyu0926@hotmail.com>
2018-01-17 21:51:55 +0800

    back

--- dao_luvx_trial/src/main/java/org/luvx/dao/impl/HelloDaoImpl.java
+++ dao_luvx_trial/src/main/java/org/luvx/dao/impl/HelloDaoImpl.java
@@ -7,6 +7,9 @@ import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 
 import org.luvx.dao.HelloDao;
 
+/**
+ * Hibernate用
+ */
 public class HelloDaoImpl extends HibernateDaoSupport implements HelloDao {
 
 	public List<Hello> findAll() {
