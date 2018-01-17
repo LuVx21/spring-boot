2bd127d4da6e31f65b8f191e2937246b642394e8
RenXie <yeyu0926@hotmail.com>
2018-01-17 21:45:30 +0800

    test

--- dao_luvx_trial/src/test/java/org/luvx/daoTest/HelloTest.java
+++ dao_luvx_trial/src/test/java/org/luvx/daoTest/HelloTest.java
@@ -3,20 +3,36 @@ package org.luvx.daoTest;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import org.luvx.dao.HelloDao;
 import org.luvx.entity.Hello;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext-dao_Hibernate.xml")
 public class HelloTest {
+
+
+    @Autowired
+    private HelloDao helloDao;
 
     @Test
     public void testFindAll() {
-        ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:applicationContext-dao_Hibernate.xml");
-        HelloDao helloDao = (HelloDao) ac.getBean("helloDao");
-
         List<Hello> list = helloDao.findAll();
         System.out.println(list.size());
     }
+
+    // @Test
+    // public void testFindAll() {
+    //     ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:applicationContext-dao_Hibernate.xml");
+    //     HelloDao helloDao = (HelloDao) ac.getBean("helloDao");
+    //
+    //     List<Hello> list = helloDao.findAll();
+    //     System.out.println(list.size());
+    // }
 }
