809d4fd01a3a8ead84259e1a597d86bdf48fede8
RenXie <yeyu0926@hotmail.com>
2018-02-08 21:20:39 +0800

    spring-boot-jsp

---
+++ spring-boot-jsp/src/test/java/org/luvx/web/TestControllerTest.java
@@ -0,0 +1,34 @@
+package org.luvx.web;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestControllerTest {
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders.standaloneSetup(new TestController()).build();
+    }
+
+    @Test
+    public void getHello1() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+}
