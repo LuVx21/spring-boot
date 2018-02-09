fb5ea3e528219272c41fc3e37b3637a62e039704
RenXie <yeyu0926@gmail.com>
2018-02-09 14:06:18 +0900

    SSM+Thymeleaf

--- sample_luvx_ssm/web_luvx_trial/src/main/java/org/luvx/controller/Hello.java
+++ sample_luvx_ssm/web_luvx_trial/src/main/java/org/luvx/controller/Hello.java
@@ -1,7 +1,9 @@
 package org.luvx.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Hello {
@@ -15,5 +17,11 @@ public class Hello {
     public String hello() {
         System.out.println("Hello World!");
         return "success";
+    }
+
+    @RequestMapping("/test")
+    public String hello(Model model, @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
     }
 }
