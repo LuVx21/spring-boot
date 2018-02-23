2dfa2e5e0ba73f77b72425a996b1c1aac091c311
RenXie <yeyu0926@hotmail.com>
2018-02-23 23:10:21 +0800

    readme modified

--- spring-boot-jsp/src/main/java/org/luvx/Application.java
+++ spring-boot-jsp/src/main/java/org/luvx/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
-    
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
