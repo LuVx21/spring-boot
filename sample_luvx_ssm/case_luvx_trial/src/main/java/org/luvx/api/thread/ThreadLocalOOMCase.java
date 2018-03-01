1a780018d17baada35717b5332c3fcaf2a5c878e
RenXie <yeyu0926@hotmail.com>
2018-03-01 22:22:31 +0800

    线程池 ThreadLocal

---
+++ sample_luvx_ssm/case_luvx_trial/src/main/java/org/luvx/api/thread/ThreadLocalOOMCase.java
@@ -0,0 +1,60 @@
+package org.luvx.api.thread;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 使用ThreadLocal 的内存溢出示例
+ * 在使用了ThreadLocal后都remove()掉以释放内存
+ */
+public class ThreadLocalOOMCase {
+
+    private static final int THREAD_LOOP_SIZE = 500;
+    private static final int MOCK_DIB_DATA_LOOP_SIZE = 10000;
+
+    private static ThreadLocal<List<User>> threadLocal = new ThreadLocal<>();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_LOOP_SIZE);
+
+        for (int i = 0; i < THREAD_LOOP_SIZE; i++) {
+            executorService.execute(() -> {
+                threadLocal.set(new ThreadLocalOOMCase().addBigList());
+                Thread t = Thread.currentThread();
+                System.out.println(Thread.currentThread().getName());
+                threadLocal.remove(); //模拟每次使用完之后都要调用remove()掉，记住是在当前线程内部调用
+            });
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        executorService.shutdown();
+    }
+
+    private List<User> addBigList() {
+        List<User> params = new ArrayList<>(MOCK_DIB_DATA_LOOP_SIZE);
+        for (int i = 0; i < MOCK_DIB_DATA_LOOP_SIZE; i++) {
+            params.add(new User("xuliugen", "password" + i, "男", i));
+        }
+        return params;
+    }
+
+    class User {
+        private String userName;
+        private String password;
+        private String sex;
+        private int age;
+
+        public User(String userName, String password, String sex, int age) {
+            this.userName = userName;
+            this.password = password;
+            this.sex = sex;
+            this.age = age;
+        }
+    }
+}
