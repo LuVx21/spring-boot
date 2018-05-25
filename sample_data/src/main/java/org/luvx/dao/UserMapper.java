c284f7a73c2dbc80d22d802f7d7e6fd2c268e56f
RenXie <yeyu0926@hotmail.com>
2018-05-25 23:16:41 +0800

    mybatis redis

--- sample_data/src/main/java/org/luvx/dao/UserMapper.java
+++ sample_data/src/main/java/org/luvx/dao/UserMapper.java
@@ -10,7 +10,7 @@ public interface UserMapper {
     void save(User user);
 
     User findById(long id);
-    // 无效更新
+
     void update(User user);
 
     void deleteById(long id);
--- sample_data/src/test/java/org/luvx/dao/UserMapperTest.java
+++ sample_data/src/test/java/org/luvx/dao/UserMapperTest.java
@@ -1,25 +1,31 @@
 package org.luvx.dao;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.luvx.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-// @ContextConfiguration({"classpath:applicationContext.xml", "classpath:applicationContext-dao.xml", "classpath:applicationContext-redis.xml"})
-// @ContextConfiguration({"classpath:applicationContext-dao.xml", "classpath:applicationContext-redis.xml"})
 @ContextConfiguration({"classpath:applicationContext.xml"})
 public class UserMapperTest {
 
     @Autowired
     private UserMapper userMapper;
 
+    @Ignore
     @Test
     public void run01() {
-        userMapper.findById(68L);
+        User user = new User("qqq", "qqqq", 25);
+        userMapper.save(user);
+        // 不使用缓存
+        userMapper.findById(1L);
+        // 使用缓存
+        User user1 = userMapper.findById(72L);
+        // false
+        System.out.println(user == user1);
     }
-
-
 }
