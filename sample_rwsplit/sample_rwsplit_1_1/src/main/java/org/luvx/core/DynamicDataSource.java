8c2f808915df321a2260430f9750a21596c27565
RenXie <yeyu0926@hotmail.com>
2018-05-27 21:56:27 +0800

    注解方式

---
+++ sample_rwsplit/sample_rwsplit_1_1/src/main/java/org/luvx/core/DynamicDataSource.java
@@ -0,0 +1,12 @@
+package org.luvx.core;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+public class DynamicDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return DynamicDataSourceHolder.getDataSouce();
+    }
+
+}
---
+++ sample_rwsplit/sample_rwsplit_1_1/src/main/java/org/luvx/core/DynamicDataSourceAspect.java
@@ -0,0 +1,27 @@
+package org.luvx.core;
+
+import java.lang.reflect.Method;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+
+@Aspect
+public class DynamicDataSourceAspect {
+
+    public void before(JoinPoint point) {
+        Object target = point.getTarget();
+        String method = point.getSignature().getName();
+        Class<?> classz = target.getClass();
+        Class<?>[] parameterTypes = ((MethodSignature) point.getSignature()).getMethod().getParameterTypes();
+        try {
+            Method m = classz.getMethod(method, parameterTypes);
+            if (m != null && m.isAnnotationPresent(DataSource.class)) {
+                DataSource data = m.getAnnotation(DataSource.class);
+                DynamicDataSourceHolder.putDataSource(data.value());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
