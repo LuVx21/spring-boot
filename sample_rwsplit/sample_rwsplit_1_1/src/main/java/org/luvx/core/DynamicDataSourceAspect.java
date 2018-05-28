79dc545ac6813c7e0ab57c1a272065daedb159e7
F.LuVx <yeyu0926@hotmail.com>
2018-05-28 12:43:59 +0800

    fix anno not finded

--- sample_rwsplit/sample_rwsplit_1_1/src/main/java/org/luvx/core/DynamicDataSourceAspect.java
+++ sample_rwsplit/sample_rwsplit_1_1/src/main/java/org/luvx/core/DynamicDataSourceAspect.java
@@ -12,10 +12,10 @@ public class DynamicDataSourceAspect {
     public void before(JoinPoint point) {
         Object target = point.getTarget();
         String method = point.getSignature().getName();
-        Class<?> classz = target.getClass();
+        Class<?>[] classz = target.getClass().getInterfaces();
         Class<?>[] parameterTypes = ((MethodSignature) point.getSignature()).getMethod().getParameterTypes();
         try {
-            Method m = classz.getMethod(method, parameterTypes);
+            Method m = classz[0].getMethod(method, parameterTypes);
             if (m != null && m.isAnnotationPresent(DataSource.class)) {
                 DataSource data = m.getAnnotation(DataSource.class);
                 DynamicDataSourceHolder.putDataSource(data.value());
