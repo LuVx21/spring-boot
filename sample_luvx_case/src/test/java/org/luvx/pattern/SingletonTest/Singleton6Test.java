769d14378fabc89786ff6790fb02602878b151eb
RenXie <yeyu0926@hotmail.com>
2018-06-25 22:53:13 +0800

    > 不被反射,反序列化破坏的单例

---
+++ sample_luvx_case/src/test/java/org/luvx/pattern/SingletonTest/Singleton6Test.java
@@ -0,0 +1,25 @@
+package org.luvx.pattern.SingletonTest;
+
+import org.junit.Test;
+import org.luvx.pattern.Singleton.Singleton6;
+
+import java.io.*;
+
+public class Singleton6Test {
+
+    @Test
+    public void run01() throws Exception {
+        Singleton6 obj1 = Singleton6.getInstance();
+
+        ObjectOutput oop = new ObjectOutputStream(new FileOutputStream("out.out"));
+        oop.writeObject(obj1);
+        oop.close();
+
+        ObjectInput oip = new ObjectInputStream(new FileInputStream("out.out"));
+        Singleton6 obj2 = (Singleton6) oip.readObject();
+        oip.close();
+
+        System.out.println(obj1 == obj2);
+    }
+
+}
