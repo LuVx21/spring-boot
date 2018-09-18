d5421fc0c24ebe745cda15842c024b4c115c0de8
RenXie <yeyu0926@hotmail.com>
2018-09-18 12:44:08 +0800

    > update ArraySerialize

--- sample_luvx_case/src/main/java/org/luvx/inter/ArraySerialize.java
+++ sample_luvx_case/src/main/java/org/luvx/inter/ArraySerialize.java
@@ -1,5 +1,6 @@
 package org.luvx.inter;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -29,15 +30,19 @@ public class ArraySerialize {
         if (str == null || str.length() == 0) {
             return new Map[0];
         }
-        String[] array = str.split("\\n");
+        // String[] array = str.split("\\n");
+        String[] array = split(str, "\\n");
+
         int length = array.length;
         Map<String, String>[] result = new Map[length];
 
         for (int i = 0; i < length; i++) {
-            String[] array1 = array[i].split(";");
+            // String[] array1 = array[i].split(";");
+            String[] array1 = split(array[i], ";");
             Map<String, String> map = new HashMap<>();
             for (int j = 0; j < array1.length; j++) {
-                map.put(array1[j].split("=")[0], array1[j].split("=")[1]);
+                // map.put(array1[j].split("=")[0], array1[j].split("=")[1]);
+                map.put(split(array1[j], "=")[0], split(array1[j], "=")[1]);
             }
             result[i] = map;
         }
@@ -61,5 +66,33 @@ public class ArraySerialize {
             sb.append(entry.getKey() + "=" + entry.getValue() + (iterator.hasNext() ? ";" : ""));
         }
         return sb.toString();
+    }
+
+    public static String[] split(String str, String regex) {
+        String[] result = null;
+        if (str.length() <= regex.length()) {
+            return new String[0];
+        } else {
+            result = new String[str.length() / (regex.length() + 1) + 1];
+        }
+
+        char[] strChars = str.toCharArray();
+        char[] regexChars = (regex == "\\n") ? new char[]{'\n'} : regex.toCharArray();
+        int startIndex = 0;
+        int resultIndex = 0;
+
+        for (int i = 0; i < strChars.length; i++) {
+            int j = 0;
+            while (i + j < strChars.length && j < regexChars.length && strChars[i + j] == regexChars[j]) {
+                j++;
+            }
+
+            if (j > 0 && i != startIndex && j == regexChars.length) {
+                result[resultIndex++] = new String(strChars, startIndex, i - startIndex);
+                startIndex = i + regexChars.length;
+            }
+        }
+        result[resultIndex++] = new String(strChars, startIndex, strChars.length - startIndex);
+        return Arrays.copyOf(result, resultIndex);
     }
 }
