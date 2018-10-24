55ea0d7b34a3fd4a272ce047c06ccb3c790d9dd1
RenXie <yeyu0926@hotmail.com>
2018-10-24 23:56:05 +0800

    单链表

--- sample_luvx_case/src/main/java/org/luvx/inter/Q3.java
+++ sample_luvx_case/src/main/java/org/luvx/inter/Q3.java
@@ -20,7 +20,7 @@ public class Q3 {
     }
 
     /**
-     * 移除奇数位节点
+     * 移除奇数位节点:双向链表
      *
      * @param head
      * @return 头节点
@@ -49,6 +49,34 @@ public class Q3 {
     }
 
     /**
+     * 移除奇数位节点:单向链表
+     *
+     * @param head
+     * @return
+     */
+    public static Node removeSingleNode1(Node head) {
+        int index = 1;
+        Node current = head;
+
+        while (current != null && current.next != null) {
+            if (index % 2 != 0) {
+                if (index == 1) {
+                    head = head.next;
+                    current = head;
+                    index++;
+                    continue;
+                }
+                current.next = current.next.next;
+                current = current.next.next;
+            } else {
+                index++;
+                continue;
+            }
+        }
+        return head;
+    }
+
+    /**
      * 遍历链表
      *
      * @param head
@@ -66,12 +94,14 @@ public class Q3 {
         Node node3 = new Node(null, node4, 3);
         Node node2 = new Node(null, node3, 2);
         Node node1 = new Node(null, node2, 1);
-
         node4.pre = node3;
         node3.pre = node2;
         node2.pre = node1;
+
         printLink(node1);
         Node head = removeSingleNode(node1);
+        printLink(head);
+        head = removeSingleNode1(head);
         printLink(head);
     }
 }
