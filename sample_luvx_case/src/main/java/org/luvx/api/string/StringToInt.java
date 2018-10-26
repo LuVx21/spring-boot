afd48c94ff21945b1b83afc81e4bbd99700070fa
zhaok2 <zhaok2@ziroom.com>
2018-10-26 21:27:02 +0800

    update

---
+++ sample_luvx_case/src/main/java/org/luvx/api/string/StringToInt.java
@@ -0,0 +1,21 @@
+package org.luvx.api.string;
+
+/**
+ * string -> int
+ */
+public class StringToInt {
+    /**
+     * @param str
+     * @return
+     */
+    public static int stringToInt(String str) {
+        char[] chars = str.toCharArray();
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- sample_luvx_commons/src/main/java/org/luvx/utils/ListPageUtils.java
+++ sample_luvx_commons/src/main/java/org/luvx/utils/ListPageUtils.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class ListPageUtil<T> {
+public class ListPageUtils<T> {
     /**
      * 原集合
      */
