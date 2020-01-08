6e3e520b3b1f0c1bd393759ae42e20fe5abea43a
F.LuVx <yeyu0926@hotmail.com>
2020-01-08 09:43:29 +0800

    udpate

--- spring-boot-kafka/src/main/java/org/luvx/old/LogProducer1.java
+++ spring-boot-kafka/src/main/java/org/luvx/old/LogProducer1.java
@@ -1,13 +1,13 @@
 package org.luvx.old;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.luvx.utils.KafkaUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * @ClassName: org.luvx._new
@@ -20,13 +20,43 @@ import java.util.Properties;
 public class LogProducer1 {
     private static final String topic = "foo";
 
+    /**
+     * 同步发送消息
+     */
     public void send(String msg) {
         Properties props = KafkaUtils.getProducerProp();
         Producer<String, String> producer = new KafkaProducer<>(props);
 
-        producer.send(new ProducerRecord<>(topic, null, null, msg));
+        Future future = producer.send(new ProducerRecord<>(topic, null, null, msg));
+        try {
+            RecordMetadata data = (RecordMetadata) future.get();
+        } catch (ExecutionException e) {
+            System.out.println(e);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
 
         log.info("生产消息:{}", msg);
         producer.flush();
+    }
+
+    /**
+     * 异步发送消息
+     */
+    public void send1(String msg) {
+        Properties props = KafkaUtils.getProducerProp();
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        producer.send(new ProducerRecord<>(topic, null, null, msg), new ProducerCallback());
+    }
+
+    private static class ProducerCallback implements Callback {
+        @Override
+        public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+            //如果Kafka返回一个错误，onCompletion方法抛出一个non null异常。
+            if (e != null) {
+                e.printStackTrace();
+            }
+        }
     }
 }
