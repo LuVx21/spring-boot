a25048bd8a69c58250fed7a0c5f3cd8855fbe940
RenXie <yeyu0926@hotmail.com>
2020-03-07 19:32:21 +0800

    基本 mapper 分成多个 mapper

---
+++ spring-boot-mybatis/src/main/java/org/luvx/common/BaseSelectMapper.java
@@ -0,0 +1,41 @@
+package org.luvx.common;
+
+import org.apache.ibatis.annotations.Param;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+public interface BaseSelectMapper<T> {
+    /**
+     * 根据id查询实体
+     *
+     * @param id
+     * @return entity
+     */
+    T selectByPrimaryKey(@Param("id") Serializable id);
+
+    /**
+     * 查询
+     *
+     * @param record
+     * @return
+     */
+    List<T> selectSelective(@Param("record") T record);
+
+    /**
+     * 批量查询
+     *
+     * @param ids
+     * @return
+     */
+    List<T> selectByPrimaryKeyList(@Param("ids") Collection<Serializable> ids);
+
+    /**
+     * 批量查询
+     *
+     * @param records
+     * @return
+     */
+    List<T> selectSelectiveList(@Param("records") Collection<T> records);
+}
