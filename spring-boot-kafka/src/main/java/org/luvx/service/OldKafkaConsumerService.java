8be744209b68977e7c28791e05efbf176e332b97
F.LuVx <yeyu0926@hotmail.com>
2020-04-02 13:29:11 +0800

    1. 监听再平衡 2. 手动提交offset

--- spring-boot-kafka/src/main/java/org/luvx/service/OldKafkaConsumerService.java
+++ spring-boot-kafka/src/main/java/org/luvx/service/OldKafkaConsumerService.java
@@ -1,16 +1,19 @@
 package org.luvx.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.PartitionInfo;
 import org.apache.kafka.common.TopicPartition;
 import org.luvx.common.config.KafkaConfig;
 import org.luvx.common.utils.KafkaUtils;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Properties;
+import java.time.LocalDateTime;
+import java.util.*;
 
 /**
  * @ClassName: org.luvx._new
@@ -29,10 +32,16 @@ public class OldKafkaConsumerService {
         Properties props = KafkaUtils.getConsumerProp();
         Consumer<String, String> consumer = new KafkaConsumer<>(props);
 
-        TopicPartition partition = new TopicPartition(KafkaConfig.topic, 0);
-        TopicPartition partition1 = new TopicPartition(KafkaConfig.topic, 1);
-        consumer.assign(Arrays.asList(partition, partition1));
-        /// consumer.seek(partition, 0);
+        List<TopicPartition> partitions = new ArrayList<>();
+        List<PartitionInfo> partitionInfos = consumer.partitionsFor(KafkaConfig.topic);
+        for (PartitionInfo info : partitionInfos) {
+            if (info.partition() <= 1) {
+                TopicPartition partition = new TopicPartition(info.topic(), info.partition());
+                /// consumer.seek(partition, 0);
+                partitions.add(partition);
+            }
+        }
+        consumer.assign(partitions);
 
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
@@ -44,23 +53,71 @@ public class OldKafkaConsumerService {
     /**
      * 消费: 异步提交
      * 已关闭自动提交
+     * <p>
+     * 配合同步提交保证最大限度的成功提交
      */
     public void get1() {
         Properties props = KafkaUtils.getConsumerProp();
-        Consumer<String, String> consumer = new KafkaConsumer<>(props);
+        try (Consumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            // 1. 不监听再平衡
+            // consumer.subscribe(Arrays.asList(KafkaConfig.topic));
+            // 2. 监听再平衡
+            consumer.subscribe(Arrays.asList(KafkaConfig.topic), new ConsumerRebalanceListener() {
+                        /**
+                         * 停止消费消息后, 再均衡前调用
+                         * @param partitions
+                         */
+                        @Override
+                        public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+                            log.info("再均衡开始:{}", LocalDateTime.now());
+                            consumer.commitSync();
+                        }
 
-        consumer.subscribe(Arrays.asList(KafkaConfig.topic));
-
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-            KafkaUtils.print(records);
-            consumer.commitAsync(
-                    (offsets, exception) -> {
-                        if (exception != null) {
-                            log.error("commit exception! offset:{}", offsets);
+                        /**
+                         * 再均衡, 开始消费消息前调用
+                         * @param partitions
+                         */
+                        @Override
+                        public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+                            log.info("再均衡结束:{}", LocalDateTime.now());
                         }
                     }
             );
+
+            // 1. 不指定offset,默认提交最大 offset
+            try {
+                while (true) {
+                    ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                    KafkaUtils.print(records);
+                    consumer.commitAsync(
+                            (offsets, exception) -> {
+                                if (exception != null) {
+                                    log.error("commit exception! offset:{}", offsets);
+                                }
+                            }
+                    );
+                }
+            } finally {
+                consumer.commitSync();
+            }
+
+            /*
+            // 2. 指定提交的偏移量
+            Map<TopicPartition, OffsetAndMetadata> offsets = new HashMap<>(8);
+            while (true) {
+                ConsumerRecords<String, String> records1 = consumer.poll(Duration.ofMillis(100));
+                // KafkaUtils.print(records);
+                for (ConsumerRecord<String, String> record : records1) {
+                    TopicPartition tp = new TopicPartition(record.topic(), record.partition());
+                    OffsetAndMetadata om = new OffsetAndMetadata(record.offset() + 1, "nothing");
+                    offsets.put(tp, om);
+                }
+                consumer.commitAsync(offsets, null);
+            }
+            */
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
