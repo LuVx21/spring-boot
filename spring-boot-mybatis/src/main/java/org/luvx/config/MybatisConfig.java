88abd525e56534b58734daf21bf05c1492bac033
RenXie <yeyu0926@hotmail.com>
2020-04-05 21:15:57 +0800

    update

--- spring-boot-mybatis/src/main/java/org/luvx/config/MybatisConfig.java
+++ spring-boot-mybatis/src/main/java/org/luvx/config/MybatisConfig.java
@@ -3,7 +3,7 @@ package org.luvx.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
-@MapperScan(basePackages = "org.luvx.mapper")
+@MapperScan(basePackages = {"org.luvx.mapper","org.luvx.anno.mapper"})
 @Configuration
 public class MybatisConfig {
 }
