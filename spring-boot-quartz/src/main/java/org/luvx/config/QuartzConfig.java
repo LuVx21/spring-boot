778036a55e0f25c83f48d838b28323e1724d3053
RenXie <yeyu0926@hotmail.com>
2020-04-28 22:47:09 +0800

    自动化执行的 job

--- spring-boot-quartz/src/main/java/org/luvx/config/QuartzConfig.java
+++ spring-boot-quartz/src/main/java/org/luvx/config/QuartzConfig.java
@@ -1,6 +1,17 @@
 package org.luvx.config;
 
+import lombok.Data;
+import org.luvx.job.BaseJob;
+import org.luvx.job.custom.CustomJob1;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.quartz.Trigger;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @ClassName: org.luvx.config
@@ -10,5 +21,76 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class QuartzConfig {
+    @Bean("methodInvokingJobDetailFactoryBean")
+    public MethodInvokingJobDetailFactoryBean methodInvokingJobDetailFactoryBean() {
+        MethodInvokingJobDetailFactoryBean factory = new MethodInvokingJobDetailFactoryBean();
+        factory.setTargetObject(new CustomJob1());
+        factory.setTargetMethod("executeNoArgs");
+        factory.setConcurrent(false);
+        return factory;
+    }
+
+    @Bean("jobDetailFactoryBean")
+    public JobDetailFactoryBean jobDetailFactoryBean() {
+        JobDetailFactoryBean factory = new JobDetailFactoryBean();
+        factory.setJobClass(BaseJob.class);
+        JobDataMap jobDataMap = new JobDataMap();
+        jobDataMap.put("customJob", new CustomJob1());
+        factory.setJobDataMap(jobDataMap);
+        factory.setDurability(true);
+        return factory;
+    }
+
+    @Bean("simpleTriggerFactoryBean")
+    public SimpleTriggerFactoryBean simpleTriggerFactoryBean(MethodInvokingJobDetailFactoryBean jobDetailFactory) {
+        SimpleTriggerFactoryBean factory = new SimpleTriggerFactoryBean();
+        factory.setJobDetail(jobDetailFactory.getObject());
+        factory.setStartDelay(1_000);
+        factory.setRepeatInterval(4_000);
+        return factory;
+    }
+
+    @Bean("cronTriggerFactoryBean")
+    public CronTriggerFactoryBean cronTriggerFactoryBean(JobDetailFactoryBean jobDetailFactoryBean) {
+        CronTriggerFactoryBean factory = new CronTriggerFactoryBean();
+        factory.setJobDetail(jobDetailFactoryBean.getObject());
+        factory.setCronExpression("0/5 * * * * ?");
+        return factory;
+    }
+
+    @Bean("schedulerFactoryBean")
+    public SchedulerFactoryBean schedulerFactoryBean(JobConfigBean bean) {
+        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+        factory.setJobDetails(bean.getJobDetails());
+        factory.setTriggers(bean.getTriggers());
+        return factory;
+    }
+
+    @Bean("jobConfigBean")
+    public JobConfigBean jobConfigBean() {
+        JobConfigBean bean = new JobConfigBean();
+
+        List<JobDetail> jobDetails = new ArrayList<>();
+        List<Trigger> triggers = new ArrayList<>();
+
+        MethodInvokingJobDetailFactoryBean factory = methodInvokingJobDetailFactoryBean();
+        jobDetails.add(factory.getObject());
+        triggers.add(simpleTriggerFactoryBean(factory).getObject());
+
+        JobDetailFactoryBean factory1 = jobDetailFactoryBean();
+        jobDetails.add(factory1.getObject());
+        triggers.add(cronTriggerFactoryBean(factory1).getObject());
+
+        bean.setJobDetails(jobDetails.stream().toArray(JobDetail[]::new));
+        bean.setTriggers(triggers.stream().toArray(Trigger[]::new));
+
+        return bean;
+    }
+
+    @Data
+    public class JobConfigBean {
+        private JobDetail[] jobDetails;
+        private Trigger[]   triggers;
+    }
 }
 
