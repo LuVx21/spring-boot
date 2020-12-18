c7cb4252051ba475e28b70d51fd42cf2da73a23e
F.LuVx <yeyu0926@hotmail.com>
2020-12-18 10:59:16 +0800

    update

--- spring-boot-web/spring-boot-websocket/src/main/java/org/luvx/websocket/Application.java
+++ spring-boot-web/spring-boot-websocket/src/main/java/org/luvx/websocket/Application.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Ren, Xie
  */
 @SpringBootApplication
-@RestController
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- spring-boot-web/spring-boot-websocket/src/main/java/org/luvx/websocket/websocket/MyWebSocket.java
+++ spring-boot-web/spring-boot-websocket/src/main/java/org/luvx/websocket/websocket/MyWebSocket.java
@@ -1,8 +1,6 @@
 package org.luvx.websocket.websocket;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
