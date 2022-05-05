1a46db0ddea4e20a20e5bd2febbfa24617b8c436
F.LuVx <yeyu0926@hotmail.com>
2022-05-05 19:40:44 +0800

    update

--- spring-boot-case/tome/src/main/java/org/luvx/tome/controller/WeChatController.java
+++ spring-boot-case/tome/src/main/java/org/luvx/tome/controller/WeChatController.java
@@ -3,11 +3,17 @@ package org.luvx.tome.controller;
 import org.luvx.tome.wechat.WXBizJsonMsgCrypt;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping(value = "/tome/wechat")
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
+@RequestMapping(value = "/tome/wechat")
 public class WeChatController {
     @Value("${wechat.corpid}")
     private String corpid;
@@ -16,9 +22,22 @@ public class WeChatController {
     @Value("${wechat.encodingAesKey}")
     String encodingAesKey;
 
-    @GetMapping("/doMsg")
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/doMsg1")
     public String a(String msg_signature, String timestamp, String nonce, String echostr) throws Exception {
         WXBizJsonMsgCrypt wxBizJsonMsgCrypt = new WXBizJsonMsgCrypt(token, encodingAesKey, corpid);
         return wxBizJsonMsgCrypt.VerifyURL(msg_signature, timestamp, nonce, echostr);
+    }
+
+    @PostMapping("doMsg")
+    public String b(String msg_signature, String timestamp, String nonce, @RequestBody String data) throws Exception {
+        WXBizJsonMsgCrypt wxBizJsonMsgCrypt = new WXBizJsonMsgCrypt(token, encodingAesKey, corpid);
+        String s = wxBizJsonMsgCrypt.DecryptMsg(msg_signature, timestamp, nonce, data);
+        log.info("明文:{}", s);
+        return s;
     }
 }
