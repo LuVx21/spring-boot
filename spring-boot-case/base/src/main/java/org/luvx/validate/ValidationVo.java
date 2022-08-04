bac79322e9bdf60e58eef7bd98168470d58eaa8b
F.LuVx <yeyu0926@hotmail.com>
2022-08-04 23:42:19 +0800

    update

--- spring-boot-case/base/src/main/java/org/luvx/validate/ValidationVo.java
+++ spring-boot-case/base/src/main/java/org/luvx/validate/ValidationVo.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
@@ -15,6 +16,7 @@ import javax.validation.groups.Default;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.Range;
+import org.luvx.validate.custom.Sex;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -26,13 +28,13 @@ import lombok.ToString;
 public class ValidationVo {
     @NotNull(message = "必须指定id", groups = {UpdateGroup.class})
     private Integer        id;
-    // @NotBlank(message = "用户名不可为blank", groups = {AddGroup.class})
-    @NotEmpty(message = "用户名不可为empty", groups = {AddGroup.class})
+    @NotBlank(message = "用户名不可为blank", groups = {AddGroup.class})
+    // @NotEmpty(message = "用户名不可为empty", groups = {AddGroup.class})
     // @NotNull(message = "用户名不可为null", groups = {AddGroup.class})
     // @Length(min = 4, max = 8, message = "用户名长度要求在{min}-{max}之间")
     private String         username;
-    @Null(message = "密码不可修改", groups = {UpdateGroup.class})
     @NotNull(message = "密码不可为null", groups = {AddGroup.class})
+    @Null(message = "密码不可修改", groups = {UpdateGroup.class})
     private String         password;
     @Email(message = "邮箱格式错误", groups = {AddGroup.class, UpdateGroup.class})
     private String         email;
@@ -43,6 +45,8 @@ public class ValidationVo {
     private Integer        age;
     @Range(min = 0, max = 1, message = "性别选择错误")
     private Integer        sex;
+    @Sex(message = "只能为男/女")
+    private String         sexName;
     @Valid
     // @NotNull(message = "voList不可为空")
     @NotEmpty(message = "voList必须有元素")
