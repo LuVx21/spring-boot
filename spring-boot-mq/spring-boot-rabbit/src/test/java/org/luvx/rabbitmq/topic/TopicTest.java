401564fa06b5cfb680946d52eaeedd01a37fd3bf
F.LuVx <yeyu0926@hotmail.com>
2022-08-04 21:35:00 +0800

    update

--- spring-boot-mq/spring-boot-rabbit/src/test/java/org/luvx/rabbitmq/topic/TopicTest.java
+++ spring-boot-mq/spring-boot-rabbit/src/test/java/org/luvx/rabbitmq/topic/TopicTest.java
@@ -1,13 +1,13 @@
 package org.luvx.rabbitmq.topic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.luvx.rabbit.topic.producer.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class TopicTest {
 
