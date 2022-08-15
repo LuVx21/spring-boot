8116ac0b85ce78b3cf1d186cd99c881102d48ccd
F.LuVx <yeyu0926@hotmail.com>
2022-08-15 17:23:39 +0800

    update

---
+++ spring-boot-case/base/src/main/java/org/luvx/web/IndexController.java
@@ -0,0 +1,24 @@
+package org.luvx.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class IndexController {
+    @RequestMapping(value = {"index", "app"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public Object index() {
+        UserVo user = new UserVo();
+        user.setUserId(10000L);
+        user.setUserName("foo");
+        user.setPassWord("bar");
+        user.setAge(18);
+        user.setValid(2);
+        return user;
+    }
+
+    @PostMapping("post")
+    public Object post(@RequestBody UserVo user) {
+        return user.toString();
+    }
+}
---
+++ spring-boot-case/base/src/main/java/org/luvx/web/UserVo.java
@@ -0,0 +1,29 @@
+package org.luvx.web;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.luvx.boot.web.enums.CommonStatusEnum;
+import org.luvx.common.enums.ext.EnumNameAnno;
+import org.luvx.common.enums.ext.EnumNameSerializer;
+
+@Getter
+@Setter
+@ToString
+public class UserVo {
+    private Long    userId;
+    private String  userName;
+    private String  passWord;
+    @JsonProperty(access = Access.WRITE_ONLY)
+    private Integer age;
+    @EnumNameAnno(CommonStatusEnum.class)
+    @JsonSerialize(using = EnumNameSerializer.class)
+    private int     valid;
+
+    public String getTest() {
+        return userName + ":" + passWord;
+    }
+}
