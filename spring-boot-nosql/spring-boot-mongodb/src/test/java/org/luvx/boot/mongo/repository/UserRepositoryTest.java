f041d1bc9a58a12afce7d8a72c683b39a0603ec6
F.LuVx <yeyu0926@hotmail.com>
2022-10-28 00:12:20 +0800

    update

--- spring-boot-nosql/spring-boot-mongodb/src/test/java/org/luvx/boot/mongo/repository/UserRepositoryTest.java
+++ spring-boot-nosql/spring-boot-mongodb/src/test/java/org/luvx/boot/mongo/repository/UserRepositoryTest.java
@@ -1,12 +1,16 @@
 package org.luvx.boot.mongo.repository;
 
-import lombok.extern.slf4j.Slf4j;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import javax.annotation.Resource;
+
 import org.junit.jupiter.api.Test;
 import org.luvx.boot.mongo.ApplicationTests;
 import org.luvx.boot.mongo.entity.User;
+import org.springframework.data.domain.Example;
 
-import javax.annotation.Resource;
-import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 class UserRepositoryTest extends ApplicationTests {
@@ -21,6 +25,7 @@ class UserRepositoryTest extends ApplicationTests {
         user.setUserName("foo");
         user.setPassword("bar");
         user.setAge(20);
+        user.setBirthday(LocalDateTime.now());
         userRepository.save(user);
 
         Optional<User> byId = userRepository.findById(id);
@@ -30,6 +35,22 @@ class UserRepositoryTest extends ApplicationTests {
         userRepository.save(user);
         log.info("find {} update {}", byId.get(), userRepository.findById(id).get());
 
-        userRepository.deleteById(id);
+        // userRepository.deleteById(id);
+    }
+
+    @Test
+    void m2() {
+        User user = new User();
+        user.setId(100L);
+        // user.setUserName("foo");
+        Optional<User> one = userRepository.findOne(
+                Example.of(user)
+        );
+        one.ifPresent(System.out::println);
+
+        System.out.println("----------");
+
+        Optional<User> byId = userRepository.findById(100L);
+        byId.ifPresent(System.out::println);
     }
 }
