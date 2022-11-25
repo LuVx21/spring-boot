56f0a859fa1eb9e77e99c3bf0a03cc1d7359f4ad
F.LuVx <yeyu0926@hotmail.com>
2022-11-26 01:23:13 +0800

    update

--- spring-boot-case/web/src/main/java/org/luvx/tools/ToolApp.java
+++ spring-boot-case/web/src/main/java/org/luvx/tools/ToolApp.java
@@ -1,15 +1,16 @@
 package org.luvx.tools;
 
+import com.github.lianjiatech.retrofit.spring.boot.config.RetrofitAutoConfiguration;
+
+import lombok.extern.slf4j.Slf4j;
 import org.luvx.tools.dao.config.DaoConfig;
-import org.luvx.tools.service.config.ServiceConfig;
 import org.luvx.tools.runner.config.RunnerConfig;
+import org.luvx.tools.service.config.ServiceConfig;
 import org.luvx.tools.web.config.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @ComponentScan({
@@ -20,7 +21,8 @@ import lombok.extern.slf4j.Slf4j;
         DaoConfig.class,
         RunnerConfig.class,
         WebConfig.class,
-        ServiceConfig.class
+        ServiceConfig.class,
+        RetrofitAutoConfiguration.class
 })
 @SpringBootApplication
 public class ToolApp {
